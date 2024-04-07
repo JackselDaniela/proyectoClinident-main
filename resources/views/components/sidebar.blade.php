901bea9360fb1094c062079cdd324c46120e20ea
JesusMiguel03 <jmdefreitas10@gmail.com>
2024-04-07 08:29:13 -0400

    fix: sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,7 +1,7 @@
 @props(['user'])
 
 @php
-    $showAlternative = ['GestionU', 'Personalizar', 'RolesP', 'Porcentajes'];
+    $showAlternative = ['GestionU', 'Personalizar', 'RolesP', 'Porcentajes', 'Roles.create', 'Roles.show'];
     $userRoles = $user->getRoleNames();
     $hasRoles = function (array $roles) use ($userRoles) {
         return (bool) array_intersect($roles, $userRoles->toArray());
